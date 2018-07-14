bbc196209228157cf1222f595e0f4d7960826b78
Phil <philmbp@PHILSMBP.local>
2018-07-14 12:34:23 -0500

    implemented destory action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,17 @@ class PortfoliosController < ApplicationController
 	  	@portfolio_item = Portfolio.find(params[:id])
 	  end
 
+	  def destroy
+	  	#perform the look up
+	  	@portfolio_item = Portfolio.find(params[:id])
+
+	  	#destroy/delete record
+	  	@portfolio_item.destroy
+
+	  	#redirect
+	    respond_to do |format|
+	      format.html { redirect_to portfolios_url, notice: 'record was removed.' }
+	      
+	  end	
+	end  
 end
